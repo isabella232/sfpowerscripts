309b7b2de5e375a0d97f54cef009c34fc3b1d7a1
rongkang666 <rongkang666@gmail.com>
2020-07-03 09:44:26 +0930

    Change Release tab Name and Content

--- packages/azpipelines/UI/ReleaseTab/ReleaseTab.tsx
+++ packages/azpipelines/UI/ReleaseTab/ReleaseTab.tsx
@@ -64,17 +64,62 @@ class PivotContent extends React.Component<{}, IPivotContentState> {
     public render(): JSX.Element {
         return (
             <div className="sample-pivot">
+                
+             
                 {
-                    !this.state.projects &&
-                    <p>Loading...</p>
+               
+
+                    `
+                    Sample Data: 
+                    
+                    {
+                        "runbook": "SFDC-Core Runbook",
+                        "version": 1,
+                        "metadata": "This runbook is to be executed befor",
+                        "schema_version": 1,
+                        "inputs": {
+                          "checklist_filepath": "/Users/alan.ly/Workspaces/devops/sfpowerscripts/packages/sfpowerscripts-cli/schema/checklist.yaml",
+                          "alias": "dev"
+                        },
+                        "tasks": [
+                          {
+                            "task": "Deactivate Sharing Rule",
+                            "id": 1,
+                            "steps": "Run sfpowerkit script /U23239\n/tDo that\nKick Some\nSKKSKS\nskdjsd\nsdkjs\n",
+                            "condition": "always | reject | continue",
+                            "status": "Done",
+                            "timeTaken": 4138,
+                            "User": "AMAC02Z10EKLVDQ",
+                            "Date": "2020-06-30T08:17:50.417Z"
+                          },
+                          {
+                            "task": "Deactivate Sharing Rule",
+                            "id": 3,
+                            "steps": "Do this }\n/tDo that\nKick Some\nSKKSKS\nskdjsd\nsdkjs\n",
+                            "runOnlyOn": "dev",
+                            "condition": "always | reject | continue",
+                            "status": "Skip",
+                            "timeTaken": 20940,
+                            "User": "AMAC02Z10EKLVDQ",
+                            "Date": "2020-06-30T08:18:11.358Z"
+                          },
+                          {
+                            "task": "task number 6",
+                            "id": 6,
+                            "steps": "Do this }\n/tDo that\nKick Some\nSKKSKS\nskdjsd\nsdkjs\n",
+                            "runOnlyOn": "dev",
+                            "condition": "always | reject | continue",
+                            "status": "Done",
+                            "timeTaken": 2722,
+                            "User": "AMAC02Z10EKLVDQ",
+                            "Date": "2020-06-30T08:27:40.594Z"
+                          }
+                        ]
+                      }
+                      `
+
                 }
-                {
-                    this.state.projects &&
-                    <Table
-                        columns={this.state.columns}
-                        itemProvider={this.state.projects}
-                    />
-                }
+                
             </div>
         );
     }
